185e8942fc68932717ef061f319aeae90b39b802
Inna <serebryakovainna@mail.ru>
2023-04-12 21:36:18 +0300

    Initial commit

---
+++ src/main/java/pro/sky/hwork4/HWork4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hwork4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HWork4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HWork4Application.class, args);
+    }
+
+}
